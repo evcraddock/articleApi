298f585b6407ba24d6ab06d79afc23f28baba5fb
Erik Craddock <erik@craddock.org>
2017-07-02 22:10:32 -0500

    filtering links by category and tags

--- src/api/link/link.controller.js
+++ src/api/link/link.controller.js
@@ -8,11 +8,26 @@ export const create = ({ body, params }, res, next) =>
     .then(success(res, 201))
     .catch(next)
 
-export const index = ({ query, select, cursor }, res, next) =>
-  Link.find(query, select, cursor)
+export const index = ({ query, select, cursor }, res, next) => {
+  let q = Link.find({}, select, cursor);
+  for(var key in query) {
+    if (key == 'categories' || key == 'tags') {
+      if (Array.isArray(query[key])) {
+        q.where(key).in(query[key])
+      } else {
+        q.where(key).in([query[key]])
+      }
+    }
+    else {
+      q.where(key).equals(query[key]);
+    }
+  }
+
+  q
     .then((links) => links.map((link) => link.view()))
     .then(success(res))
     .catch(next)
+  };
 
 export const show = ({ params }, res, next) =>
   Link.findById(params.id)
